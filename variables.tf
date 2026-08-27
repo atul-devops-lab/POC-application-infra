d9f3d7320bd201746bad28a2d43eeea9efd572d6
Atul Chambial <107387752+AtulChambial@users.noreply.github.com>
2026-08-27 12:55:58 +0530

    Add Terraform variables for AWS resources

--- variables.tf
+++ variables.tf
@@ -1,1 +1,23 @@
+variable "aws_region" {
+  description = "AWS region for all resources"
+  type        = string
+  default     = "ap-south-1"
+}
 
+variable "environment" {
+  description = "Environment name, used in tags and resource naming"
+  type        = string
+  default     = "lab"
+}
+
+variable "cluster_name" {
+  description = "Name of the EKS cluster"
+  type        = string
+  default     = "atul-devops-lab-eks"
+}
+
+variable "cluster_version" {
+  description = "Kubernetes version for the EKS cluster"
+  type        = string
+  default     = "1.31"
+}
