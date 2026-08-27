060c2c7f813c35c27ecd5e3db0de26ea996469fa
Atul Chambial <107387752+AtulChambial@users.noreply.github.com>
2026-08-27 12:40:40 +0530

    Configure Terraform backend with S3

--- backend.tf
+++ backend.tf
@@ -1,1 +1,9 @@
-
+terraform {
+  backend "s3" {
+    bucket       = "atul-devops-lab-tfstate"
+    key          = "eks/terraform.tfstate"
+    region       = "ap-south-1"
+    encrypt      = true
+    use_lockfile = true
+  }
+}
