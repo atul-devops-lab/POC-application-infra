adf45649964e1006463da83fd90320c4bde941c1
Atul Chambial <107387752+AtulChambial@users.noreply.github.com>
2026-09-14 11:51:29 +0530

    Refactor helm provider configuration syntax

--- bootstrap/providers.tf
+++ bootstrap/providers.tf
@@ -23,13 +23,13 @@ data "aws_eks_cluster" "this" {
 }
 
 provider "helm" {
-  kubernetes {
+  kubernetes = {
     host                   = data.aws_eks_cluster.this.endpoint
     cluster_ca_certificate = base64decode(
       data.aws_eks_cluster.this.certificate_authority[0].data
     )
 
-    exec {
+    exec = {
       api_version = "client.authentication.k8s.io/v1beta1"
       command     = "aws"
 
